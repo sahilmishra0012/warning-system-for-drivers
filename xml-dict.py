44bc9aa5593a7b100ee1239fdf270f2f5f63f6ba
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-03 18:54:40 +0530

    New Changes

--- xml-dict.py
+++ xml-dict.py
@@ -9,5 +9,5 @@ annotations_paths = glob.glob( '*.xml' )
 for xmlfile in annotations_paths:
     x=xmltodict.parse(open( xmlfile , 'rb' ))
     for i in x['annotation']['object']:
-        print(i)
+        print(i['name'],i['bndbox']['xmin'],i['bndbox']['ymin'],i['bndbox']['xmax'],i['bndbox']['ymax'])
         print("\n")
