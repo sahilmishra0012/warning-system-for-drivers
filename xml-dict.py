885d7de6e4ad1a0cd44bdd7328286c7f473c18c4
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-03 16:56:24 +0530

    New Changes

--- xml-dict.py
+++ xml-dict.py
@@ -5,16 +5,9 @@ import glob
 
 bboxes = []
 classes_raw = []
-annotations_paths = glob.glob( '/.xml' )
+annotations_paths = glob.glob( '*.xml' )
 for xmlfile in annotations_paths:
-    x = xmltodict.parse( open( xmlfile , 'rb' ) )
-    bndbox = x[ 'annotation' ][ 'object' ][ 'bndbox' ]
-    bndbox = np.array([ int(bndbox[ 'xmin' ]) , int(bndbox[ 'ymin' ]) , int(bndbox[ 'xmax' ]) , int(bndbox[ 'ymax' ]) ])
-    bndbox2 = [ None ] * 4
-    bndbox2[0] = bndbox[0]
-    bndbox2[1] = bndbox[1]
-    bndbox2[2] = bndbox[2]
-    bndbox2[3] = bndbox[3]
-    bndbox2 = np.array( bndbox2 )
-    bboxes.append( bndbox2 )
-    classes_raw.append( x[ 'annotation' ][ 'object' ][ 'name' ] )
+    x=xmltodict.parse(open( xmlfile , 'rb' ))
+    for i in x['annotation']['object']:
+        print(i)
+        print("\n")
