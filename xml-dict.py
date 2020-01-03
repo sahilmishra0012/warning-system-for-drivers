d8f0c64e23753199f77902e3c42bfbdc82b9d235
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-03 20:23:45 +0530

    New Changes

--- xml-dict.py
+++ xml-dict.py
@@ -2,12 +2,26 @@ import xmltodict
 import os
 import numpy as np
 import glob
+from PIL import Image
+from PIL import ImageDraw
+import cv2
 
-bboxes = []
-classes_raw = []
+img=cv2.imread('0001999.jpg')
 annotations_paths = glob.glob( '*.xml' )
 for xmlfile in annotations_paths:
     x=xmltodict.parse(open( xmlfile , 'rb' ))
     for i in x['annotation']['object']:
-        print(i['name'],i['bndbox']['xmin'],i['bndbox']['ymin'],i['bndbox']['xmax'],i['bndbox']['ymax'])
-        print("\n")
+        Xminvalue=int(i['bndbox']['xmin'])
+        Xmaxvalue=int(i['bndbox']['xmax'])
+        Yminvalue=int(i['bndbox']['ymin'])
+        Ymaxvalue=int(i['bndbox']['ymax'])
+        img = cv2.rectangle(img,(Xminvalue,Yminvalue),(Xmaxvalue,Ymaxvalue),(0,255,0),3)
+    cv2.imshow('Image Window',img)
+    k=cv2.waitKey(0)
+
+    if k==27:#  27 is for escape character
+        cv2.destroyAllWindows()# To destroy all the windows
+    # Write Image when s is pressed.
+    elif k==ord('s'):
+        cv2.imwrite('bird.jpg',img)
+        cv2.destroyAllWindows()# To destroy all the windows
