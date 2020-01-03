b57323db69feb55309e6c0b85ffae9ab3ac256fe
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-03 21:53:48 +0530

    New Changes

--- Test_Folder/test1.py
+++ Test_Folder/test1.py
@@ -7,19 +7,19 @@ img_paths=glob.glob('*.jpg')
 for xmlfile,imgfile in zip(sorted(annotations_paths),sorted(img_paths)):
     x=xmltodict.parse(open( xmlfile , 'rb' ))
     img=cv2.imread(imgfile)
-    width = int(img.shape[1] * 50 / 100)
-    height = int(img.shape[0] * 50 / 100)
+    width = int(img.shape[1] * 65 / 100)
+    height = int(img.shape[0] * 65 / 100)
     dim = (width, height)
     # resize image
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
     for i in x['annotation']['object']:
-        Xminvalue=int(int(i['bndbox']['xmin'])* 50 / 100)
-        Xmaxvalue=int(int(i['bndbox']['xmax'])* 50 / 100)
-        Yminvalue=int(int(i['bndbox']['ymin'])* 50 / 100)
-        Ymaxvalue=int(int(i['bndbox']['ymax'])* 50 / 100)
+        Xminvalue=int(int(i['bndbox']['xmin'])* 65 / 100)
+        Xmaxvalue=int(int(i['bndbox']['xmax'])* 65 / 100)
+        Yminvalue=int(int(i['bndbox']['ymin'])* 65 / 100)
+        Ymaxvalue=int(int(i['bndbox']['ymax'])* 65 / 100)
         name=i['name']
         img = cv2.rectangle(img,(Xminvalue,Yminvalue),(Xmaxvalue,Ymaxvalue),(255,255,0),1)
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img,name,(Xminvalue,Yminvalue), font, 0.5,(255,255,0),1,cv2.LINE_AA)
     cv2.imshow('Image Window',img)
-    k=cv2.waitKey(4000)
+    k=cv2.waitKey(7)
