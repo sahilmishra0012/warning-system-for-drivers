6f1f990fca5aaaf4a21e3d5356147fecef560cfb
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-03 15:15:25 +0530

    New Changes

--- xml-dict.py
+++ xml-dict.py
@@ -1,9 +1,11 @@
 import xmltodict
 import os
+import numpy as np
+import glob
 
 bboxes = []
 classes_raw = []
-annotations_paths = glob.glob( 'training_images/*.xml' )
+annotations_paths = glob.glob( '/.xml' )
 for xmlfile in annotations_paths:
     x = xmltodict.parse( open( xmlfile , 'rb' ) )
     bndbox = x[ 'annotation' ][ 'object' ][ 'bndbox' ]
@@ -13,6 +15,6 @@ for xmlfile in annotations_paths:
     bndbox2[1] = bndbox[1]
     bndbox2[2] = bndbox[2]
     bndbox2[3] = bndbox[3]
-    bndbox2 = np.array( bndbox2 ) / input_dim
+    bndbox2 = np.array( bndbox2 )
     bboxes.append( bndbox2 )
     classes_raw.append( x[ 'annotation' ][ 'object' ][ 'name' ] )
