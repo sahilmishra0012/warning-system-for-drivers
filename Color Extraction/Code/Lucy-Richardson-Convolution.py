5d43253bbc374a7e92369a95db2669c537d61940
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-07 10:45:29 +0530

    New Changes

--- Color Extraction/Code/Lucy-Richardson-Convolution.py
+++ Color Extraction/Code/Lucy-Richardson-Convolution.py
@@ -4,16 +4,18 @@ import cv2
 from scipy.signal import convolve2d
 from skimage import color, data, restoration
 
-img=cv2.imread('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/Color Extraction/Data/0003660.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(img, 5)
+from skimage import color, data, restoration,io
 
+img = io.imread('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/Color Extraction/Data/0003660.jpg')
+img=color.rgb2gray(img)
 psf = np.ones((5, 5)) / 25
 img = convolve2d(img, psf, 'same')
-img += 0.1 * img.std() * np.random.standard_normal(img.shape)
-img = restoration.richardson_lucy(img, psf, iterations=200)
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-cv2.imshow("Image Window",img)
+astro_noisy = img.copy()
+
+astro_noisy += (np.random.poisson(lam=25, size=img.shape) - 10) / 255
+
+deconv = restoration.richardson_lucy(astro_noisy, psf,30)
+deconv=color.gray2rgb(deconv)
+cv2.imshow("Image Window",deconv)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
