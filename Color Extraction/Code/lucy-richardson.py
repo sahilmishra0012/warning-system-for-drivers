a8ea29fda686016816a776cba11586861b8839c7
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-09 23:06:53 +0530

    New Changes

--- Color Extraction/Code/lucy-richardson.py
+++ Color Extraction/Code/lucy-richardson.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.signal import convolve2d as conv2
-
 from skimage import color, data, restoration
 
 
@@ -12,11 +10,6 @@ PSF = fspecial('disk', 8);
 noise_mean = 0;
 noise_var = 0.0001;
 estimated_nsr = noise_var / var(I(:));
-
-astro = color.rgb2gray(data.astronaut())
-
-psf = np.ones((5, 5)) / 25
-astro = conv2(astro, psf, 'gaussian')
 
 # Restore Image using Richardson-Lucy algorithm
 deconvolved_RL = restoration.richardson_lucy(astro_noisy, psf, iterations=30)
