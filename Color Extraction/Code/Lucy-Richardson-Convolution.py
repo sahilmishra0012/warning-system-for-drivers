04c7583cce3964b0a56f7c768721931e22eef142
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-09 18:35:04 +0530

    New Changes

--- Color Extraction/Code/Lucy-Richardson-Convolution.py
+++ Color Extraction/Code/Lucy-Richardson-Convolution.py
@@ -1,21 +1,24 @@
 import numpy as np
 import cv2
 
-from scipy.signal import convolve2d
+from scipy.signal import convolve3d
 from skimage import color, data, restoration
 
 from skimage import color, data, restoration,io
 
 img = io.imread('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/Color Extraction/Data/0003660.jpg')
-img=color.rgb2gray(img)
+# img=color.rgb2gray(img)
 psf = np.ones((5, 5)) / 25
-img = convolve2d(img, psf, 'same')
+
+img = convolve3d(img, psf, 'same')
 astro_noisy = img.copy()
 
 astro_noisy += (np.random.poisson(lam=25, size=img.shape) - 10) / 255
 
+
+
 deconv = restoration.richardson_lucy(astro_noisy, psf,30)
-deconv=color.gray2rgb(deconv)
+# deconv=color.gray2rgb(deconv)
 cv2.imshow("Image Window",deconv)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
