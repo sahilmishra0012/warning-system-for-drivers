5b1150c66a0d4502a2356ba651a576a6761077b1
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-09 20:53:10 +0530

    New Changes

--- Color Extraction/Code/median_filtering.py
+++ Color Extraction/Code/median_filtering.py
@@ -5,11 +5,6 @@ img=cv2.imread('image.jpg')
 img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
 img = cv2.medianBlur(img, 3)
 
-# kernel=np.array([[-1,-1,-1],
-#                 [-1,9,-1],
-#                 [-1,-1,-1]])
-
-# img=cv2.filter2D(img,-1,kernel)
 img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)
 cv2.imshow('Image',img)
 cv2.waitKey(0)
