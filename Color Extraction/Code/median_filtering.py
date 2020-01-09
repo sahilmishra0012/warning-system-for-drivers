ba0131df4565c63d96a72fb09c426607653f6302
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-09 20:35:33 +0530

    New Changes

--- Color Extraction/Code/median_filtering.py
+++ Color Extraction/Code/median_filtering.py
@@ -1,6 +1,15 @@
 import cv2
+import numpy as np
 
-img=cv2.imread('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/Color Extraction/Data/0003660.jpg')
-img = cv2.medianBlur(img, 1)
+img=cv2.imread('image.jpg')
+img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
+img = cv2.medianBlur(img, 5)
+
+# kernel=np.array([[-1,-1,-1],
+#                 [-1,9,-1],
+#                 [-1,-1,-1]])
+
+# img=cv2.filter2D(img,-1,kernel)
+img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)
 cv2.imshow('Image',img)
 cv2.waitKey(0)
