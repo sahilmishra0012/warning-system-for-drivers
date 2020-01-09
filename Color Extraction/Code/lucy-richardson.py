578fd563262338bacddfa1987526beaf49e86dea
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 01:44:24 +0530

    New Changes

--- Color Extraction/Code/lucy-richardson.py
+++ Color Extraction/Code/lucy-richardson.py
@@ -1,26 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 
-from scipy.signal import convolve2d as conv2
+from scipy.signal import convolve2d
+from skimage import color, data, restoration
 
-from skimage import color, data, restoration,io
+img=cv2.imread('image.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.medianBlur(img, 5)
 
-astro = cv2.imread('image.jpg')
-astro=cv2.cvtColor(astro,cv2.COLOR_BGR2GRAY)
-
-psf = np.ones((3, 3)) /9
-astro = conv2(astro, psf, 'same')
-# Add Noise to Image
-astro_noisy = astro.copy()
-
-# Restore Image using Richardson-Lucy algorithm
-deconvolved_RL = restoration.richardson_lucy(astro_noisy, psf, iterations=110)
-cv2.imshow('Image',deconvolved_RL)
+psf = np.ones((3, 3)) / 9
+img = convolve2d(img, psf, 'same')
+img += 0.1 * img.std() * np.random.standard_normal(img.shape)
+img = restoration.richardson_lucy(img, psf, iterations=30)
+cv2.imshow("Image Window",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
 
 # % Load image
 # I = im2double(imread('IMG_REAL_motion_blur.PNG'));
