36611703ba91c3028a400b70e20d9461f8cae610
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 09:00:02 +0530

    New Changes

--- Color Extraction/Code/lucy-richardson.py
+++ Color Extraction/Code/lucy-richardson.py
@@ -1,30 +1,21 @@
-import numpy as np
-import cv2
+# import numpy as np
+# import cv2
+# from scipy import ndimage
 
-from scipy.signal import convolve2d
-from skimage import color, data, restoration
+# from scipy.signal import convolve2d
+# from skimage import color, data, restoration
+# from skimage.morphology import disk, dilation
+#     # x = dilation(x, selem=mask)
+# img=cv2.imread('image.jpg')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img = cv2.medianBlur(img, 5)
 
-img=cv2.imread('image.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(img, 5)
-
-psf = np.ones((3, 3)) / 9
-img = convolve2d(img, psf, 'same')
-img += 0.1 * img.std() * np.random.standard_normal(img.shape)
-img = restoration.richardson_lucy(img, psf, iterations=30)
-cv2.imshow("Image Window",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# % Load image
-# I = im2double(imread('IMG_REAL_motion_blur.PNG'));
-# figure(1); imshow(I); title('Source image');
-
-# %PSF
-# PSF = fspecial('motion', 14, 0);
-# noise_mean = 0;
-# noise_var = 0.0001;
-# estimated_nsr = noise_var / var(I(:));
-
-# I = edgetaper(I, PSF);
-# figure(2); imshow(deconvwnr(I, PSF, estimated_nsr)); title('Result');
+# psf = disk(5)
+# noise_mean=0
+# noise_var=0.0001
+# estimated_nsr = noise_var / ndimage.variance(img)
+# # img += 0.1 * img.std() * np.random.standard_normal(img.shape)
+# img = restoration.richardson_lucy(img, estimated_nsr, iterations=30)
+# cv2.imshow("Image Window",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
