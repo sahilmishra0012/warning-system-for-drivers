dc43fce428e5757d1c4118267619d3fecf1c78f6
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 11:37:32 +0530

    New Changes

--- Color Extraction/Code/image_deblur/deblur.py
+++ Color Extraction/Code/image_deblur/deblur.py
@@ -8,12 +8,13 @@ img = cv2.medianBlur(img, 3)    # Median Filtering
 Y,Cr,Cb = cv2.split(img) # Channel Splitting
 
 # Histogram Equalization of each channel
-output_Y = cv2.equalizeHist(Y)
-output_Cr = cv2.equalizeHist(Cr)
-output_Cb = cv2.equalizeHist(Cb)
+output_Y = cv2.equalizeHist(Y)  # Luma Channel
+output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
+output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
 
-img = cv2.merge((output_Y, output_Cr, output_Cb))
-img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)
+img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels
+
+img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)   # YCrCb to RGB
 cv2.imshow('Image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
