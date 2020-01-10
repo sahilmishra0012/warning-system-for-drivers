763af8f0147d8f9b432af5e151531db7ecf01676
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 13:26:35 +0530

    New Changes

--- Color Extraction/Code/Color Segmentation/color_segment.py
+++ Color Extraction/Code/Color Segmentation/color_segment.py
@@ -1,8 +1,10 @@
 from ImagePreprocess import ImagePreprocess
 import cv2
 img=ImagePreprocess('image.jpg')
-
 temp_img=img.deblur()
-cv2.imshow('Image',temp_img)
+Y,Cr,Cb = cv2.split(temp_img) # Channel Splitting
+ret, thresh2 = cv2.threshold(Cr, 0, 255, cv2.THRESH_BINARY_INV) 
+
+cv2.imshow('Image',thresh2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
