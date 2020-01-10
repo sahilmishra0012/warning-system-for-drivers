03d40dc6c63fa01c69776b25f366e002b0e60d29
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 11:51:03 +0530

    New Changes

--- Color Extraction/Code/Image_Preprocess/ImagePreprocess.py
+++ Color Extraction/Code/Image_Preprocess/ImagePreprocess.py
@@ -1,37 +1,28 @@
 import cv2
-
 class ImagePreprocess:
     def __init__(self,file_name):
         self.file=file_name
-        self.img=cv2.imread(file)
+        self.img=cv2.imread(file)   # Read Image File
 
-    def rgb_ycrcb(file):
+    def rgb_ycrcb():    # RGB to YCrCb
         temp_img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
         return temp_img
 
-    def ycrcb_rgb(file):
+    def ycrcb_rgb():    # YCrCb to RGB
         temp_img=cv2.cvtColor(img,cv2.COLOR_YCrCb2RGB)
         return temp_img
 
-    def ycrcb_rgb(file):
-        temp_img=cv2.cvtColor(img,cv2.COLOR_YCrCb2RGB)
+    def median_blur():
+        temp_img = cv2.medianBlur(img, 3)    # Median Filtering
         return temp_img
 
-img=cv2.imread('image.jpg')
-img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)   # RGB to YCrCb
+    def histogram_equalize():
+        Y,Cr,Cb = cv2.split(img) # Channel Splitting
 
-img = cv2.medianBlur(img, 3)    # Median Filtering
+        # Histogram Equalization of each channel
+        output_Y = cv2.equalizeHist(Y)  # Luma Channel
+        output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
+        output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
 
-Y,Cr,Cb = cv2.split(img) # Channel Splitting
-
-# Histogram Equalization of each channel
-output_Y = cv2.equalizeHist(Y)  # Luma Channel
-output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
-output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
-
-img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels
-
-img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)   # YCrCb to RGB
-cv2.imshow('Image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        temp_img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels\
+        return temp_img
