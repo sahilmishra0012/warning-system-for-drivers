38c0faa2e24490103d1c1755bed170bfcd34ffa6
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 19:23:24 +0530

    New Changes

--- Color Extraction/Code/Shape Filtering/match_template.py
+++ Color Extraction/Code/Shape Filtering/match_template.py
@@ -1,17 +1,20 @@
 from ImagePreprocess import ImagePreprocess
 import cv2
 import numpy as np
+immg=cv2.imread('image.jpg')
 img=ImagePreprocess('image.jpg')
 temp_img=img.deblur()
 Y,Cr,Cb = cv2.split(temp_img) # Channel Splitting
-ret, thresh2 = cv2.threshold(Cr, 80, 255, cv2.THRESH_BINARY_INV)
+ret, thresh2 = cv2.threshold(Cr, 80, 255, cv2.THRESH_BINARY)
 template=cv2.imread('temp.jpg')
 template=cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+w, h = template.shape[::-1]
+print(template.shape)
 res = cv2.matchTemplate(thresh2,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.5
+threshold = 0.24
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-    cv2.rectangle(thresh2, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-cv2.imshow('Image',template)
+    cv2.rectangle(immg, pt, (pt[0] + w, pt[1] + h), (255,255,255), 0)
+cv2.imshow('Image',immg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
