d03d57771cdec604eb4bbfea5ff9adc31ada3661
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 11:40:52 +0530

    New Changes

---
+++ Color Extraction/Code/Color Segmentation/color_segment.py
@@ -0,0 +1,20 @@
+import cv2
+
+img=cv2.imread('image.jpg')
+img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)   # RGB to YCrCb
+
+img = cv2.medianBlur(img, 3)    # Median Filtering
+
+Y,Cr,Cb = cv2.split(img) # Channel Splitting
+
+# Histogram Equalization of each channel
+output_Y = cv2.equalizeHist(Y)  # Luma Channel
+output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
+output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
+
+img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels
+
+img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)   # YCrCb to RGB
+cv2.imshow('Image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
