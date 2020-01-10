ece9be1f810a5625ac50013b77cda60a0b8185df
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-10 11:41:25 +0530

    New Changes

--- Color Extraction/Code/Color Segmentation/color_segment.py
+++ Color Extraction/Code/Color Segmentation/color_segment.py
@@ -1,20 +1,17 @@
 import cv2
+import numpy as np
+img = cv2.imread("image.jpg")
+img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
 
-img=cv2.imread('image.jpg')
-img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)   # RGB to YCrCb
+Y,Cr,Cb = cv2.split(img)
 
-img = cv2.medianBlur(img, 3)    # Median Filtering
+output_Y = cv2.equalizeHist(Y)
+output_Cr = cv2.equalizeHist(Cr)
+output_Cb = cv2.equalizeHist(Cb)
+img = cv2.merge((output_Y, output_Cr, output_Cb))
 
-Y,Cr,Cb = cv2.split(img) # Channel Splitting
+ret, thresh2 = cv2.threshold(output_Cr, 200, 255, cv2.THRESH_BINARY) 
 
-# Histogram Equalization of each channel
-output_Y = cv2.equalizeHist(Y)  # Luma Channel
-output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
-output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
-
-img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels
-
-img=cv2.cvtColor(img,cv2.COLOR_YCrCb2BGR)   # YCrCb to RGB
-cv2.imshow('Image',img)
+cv2.imshow('Image',thresh2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
