35af118afa395a65193c2a692c6bb71226b2bac0
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-22 21:24:21 +0530

    New Changes

--- Tensorflow_Object_Detection_API/xml_to_csv.py
+++ Tensorflow_Object_Detection_API/xml_to_csv.py
@@ -5,24 +5,26 @@ import xml.etree.ElementTree as ET
 xml_list = []
 path = '/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/data'
 
-annotations_paths = [f for f in glob.glob(path + "/**/*.xml", recursive=True)]
+annotations_paths = [f for f in glob.glob(path + "/*.xml", recursive=True)]
 
 
 for xmlfile in sorted(annotations_paths):
-    tree=ET.parse(xml_file)
+    xml_list = []
+    tree=ET.parse(xmlfile)
     root = tree.getroot()
     for member in root.findall('object'):
+        # print(member)
         value = (root.find('filename').text,
                 int(root.find('size')[0].text),
                 int(root.find('size')[1].text),
                 member[0].text,
-                int(member[4][0].text),
-                int(member[4][1].text),
-                int(member[4][2].text),
-                int(member[4][3].text)
+                int(member[1][0].text),
+                int(member[1][1].text),
+                int(member[1][2].text),
+                int(member[1][3].text)
                 )
         xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
-    xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None)
+    xml_df.to_csv((xmlfile+'_labels.csv'), index=None)
 
