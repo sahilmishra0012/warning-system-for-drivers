229a0c3d4ee7e18ff3019727c804f5aff02cb9ca
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-23 18:56:59 +0530

    New Changes

--- Tensorflow_Object_Detection_API/xml_to_csv.py
+++ Tensorflow_Object_Detection_API/xml_to_csv.py
@@ -4,17 +4,15 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 xml_list = []
-path = '/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Data'
+path = '/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Data/Data1'
 
 annotations_paths = [f for f in glob.glob(path + "/**/**/*.xml", recursive=True)]
-print(annotations_paths)
 xml_list = []
-for xmlfile in sorted(annotations_paths):
+for xmlfile in tqdm(sorted(annotations_paths)):
     tree=ET.parse(xmlfile)
     root = tree.getroot()
     for member in root.findall('object'):
-        # print(member)
-        value = (root.find('filename').text,
+        value = (xmlfile[:-3]+"jpg",
                 int(root.find('size')[0].text),
                 int(root.find('size')[1].text),
                 member[0].text,
@@ -24,8 +22,6 @@ for xmlfile in sorted(annotations_paths):
                 int(member[1][3].text)
                 )
         xml_list.append(value)
-        print(value)
-    # column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    # xml_df = pd.DataFrame(xml_list, columns=column_name)
-    # xml_df.to_csv((xmlfile+'_labels.csv'), index=None)
-
+column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+xml_df = pd.DataFrame(xml_list, columns=column_name)
+xml_df.to_hdf(('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Data/labels1.h5'),key='df', mode='w')
