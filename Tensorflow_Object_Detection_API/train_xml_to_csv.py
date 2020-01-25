3ecc64b60caa7ef0332397a8ac71c93e22b6f3d6
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-25 18:51:19 +0530

    New Changes

---
+++ Tensorflow_Object_Detection_API/train_xml_to_csv.py
@@ -0,0 +1,26 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+from tqdm import tqdm
+train_files=pd.read_csv('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/train.txt',sep='/',header=None)
+
+xml_list = []
+for xmlfile in tqdm(train_files.iterrows()):
+    xml_path='/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Annotations/'+xmlfile[1][0]+'/'+xmlfile[1][1]+'/'+xmlfile[1][2]+'.xml'
+    tree=ET.parse(xml_path)
+    root = tree.getroot()
+    for member in root.findall('object'):
+        value = ('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/JPEGImages/'+xmlfile[1][0]+'/'+xmlfile[1][1]+'/'+root.find('filename').text,
+                int(root.find('size')[0].text),
+                int(root.find('size')[1].text),
+                member[0].text,
+                int(member[1][0].text),
+                int(member[1][1].text),
+                int(member[1][2].text),
+                int(member[1][3].text)
+                )
+        xml_list.append(value)
+column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+xml_df = pd.DataFrame(xml_list, columns=column_name)
+xml_df.to_csv(('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/train_labels.csv'),index=None)
--- Tensorflow_Object_Detection_API/validation_xml_to_csv.py
+++ Tensorflow_Object_Detection_API/validation_xml_to_csv.py
@@ -3,16 +3,15 @@ import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
-xml_list = []
-path = '/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Data/Data1'
+train_files=pd.read_csv('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/val.txt',sep='/',header=None)
 
-annotations_paths = [f for f in glob.glob(path + "/**/**/*.xml", recursive=True)]
 xml_list = []
-for xmlfile in tqdm(sorted(annotations_paths)):
-    tree=ET.parse(xmlfile)
+for xmlfile in tqdm(train_files.iterrows()):
+    xml_path='/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Annotations/'+xmlfile[1][0]+'/'+xmlfile[1][1]+'/'+xmlfile[1][2]+'.xml'
+    tree=ET.parse(xml_path)
     root = tree.getroot()
     for member in root.findall('object'):
-        value = (xmlfile[:-3]+"jpg",
+        value = ('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/JPEGImages/'+xmlfile[1][0]+'/'+xmlfile[1][1]+'/'+root.find('filename').text,
                 int(root.find('size')[0].text),
                 int(root.find('size')[1].text),
                 member[0].text,
@@ -24,4 +23,4 @@ for xmlfile in tqdm(sorted(annotations_paths)):
         xml_list.append(value)
 column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
 xml_df = pd.DataFrame(xml_list, columns=column_name)
-xml_df.to_hdf(('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/Data/labels1.h5'),key='df', mode='w')
+xml_df.to_csv(('/home/samthekiller/Downloads/Smart India Hackathon/INTEL/data/IDD_Detection/val_labels.csv'),index=None)
