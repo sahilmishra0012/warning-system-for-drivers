78110a11569c3f8fd5216c4c4307bcbde07f1d4f
Sahil Mishra <sahilmishra0012@outlook.com>
2020-01-31 09:32:38 +0530

    New Changes

--- Image Segmentation Test Codes/Color Segmentation/ImagePreprocess.py
+++ Image Segmentation Test Codes/Color Segmentation/ImagePreprocess.py
@@ -1,19 +1,42 @@
 import cv2
 import numpy as np
-class ImagePreprocess:
-    def __init__(self,file_name):
-        self.img=cv2.imread(file_name)   # Read Image File
+import matplotlib.pyplot as plt
+from math import sqrt
+from skimage.feature import blob_dog, blob_log, blob_doh
 
-    def deblur(self):  
-        temp_img=cv2.cvtColor(self.img,cv2.COLOR_BGR2YCrCb) # RGB to YCrCb
-        temp_img = cv2.medianBlur(temp_img, 5)    # Median Filtering
-        Y,Cr,Cb = cv2.split(temp_img) # Channel Splitting
 
-        # Histogram Equalization of each channel
-        output_Y = cv2.equalizeHist(Y)  # Luma Channel
-        output_Cr = cv2.equalizeHist(Cr)  # Red Chroma Channel
-        output_Cb = cv2.equalizeHist(Cb)  # Blue Chroma Channel
 
-        temp_img = cv2.merge((output_Y, output_Cr, output_Cb))   # Merge channels
-        temp_img=cv2.cvtColor(temp_img,cv2.COLOR_YCrCb2BGR) #   YCrCb to RGB
-        return temp_img
+def constrastLimit(image):
+    img_hist_equalized = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    channels = cv2.split(img_hist_equalized)
+    channels[0] = cv2.equalizeHist(channels[0])
+    img_hist_equalized = cv2.merge(channels)
+    img_hist_equalized = cv2.cvtColor(img_hist_equalized, cv2.COLOR_YCrCb2BGR)
+    return img_hist_equalized
+
+def LaplacianOfGaussian(image):
+    LoG_image = cv2.GaussianBlur(image, (3,3), 0) 
+    gray = cv2.cvtColor( LoG_image, cv2.COLOR_BGR2GRAY)
+    LoG_image = cv2.Laplacian( gray, cv2.CV_8U,3,3,2)
+    LoG_image = cv2.convertScaleAbs(LoG_image)
+    return LoG_image
+    
+def binarization(image):
+    thresh = cv2.threshold(image,32,255,cv2.THRESH_BINARY)[1]
+    return thresh
+
+def preprocess_image(image):
+    image = constrastLimit(image)
+    image = LaplacianOfGaussian(image)
+    image = binarization(image)
+    return image
+
+def main():
+    img=cv2.imread('image1.jpg')
+    img=preprocess_image(img)
+    cv2.imshow('Image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
