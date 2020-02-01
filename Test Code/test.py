a3e61d57806235a78357fbdd2b89f48a7102e9a6
Sahil Mishra <sahilmishra0012@outlook.com>
2020-02-01 10:22:57 +0530

    ]New Changes

--- Test Code/test.py
+++ Test Code/test.py
@@ -8,25 +8,12 @@ import argparse
 import os
 import math
 
-SIGNS = ["ERROR",
-        "STOP",
-        "TURN LEFT",
-        "TURN RIGHT",
-        "DO NOT TURN LEFT",
-        "DO NOT TURN RIGHT",
-        "ONE WAY",
-        "SPEED LIMIT",
-        "OTHER"]
-
-# Clean all previous file
 def clean_images():
 	file_list = os.listdir('./')
 	for file_name in file_list:
 		if '.png' in file_name:
 			os.remove(file_name)
 
-
-### Preprocess image
 def constrastLimit(image):
     img=cv2.cvtColor(image,cv2.COLOR_BGR2YCrCb)
     R, G, B = cv2.split(img)
@@ -40,15 +27,14 @@ def constrastLimit(image):
     return img
 
 def LaplacianOfGaussian(image):
-    LoG_image = cv2.GaussianBlur(image, (3,3), 0)           # paramter 
+    LoG_image = cv2.GaussianBlur(image, (3,3), 0)
     gray = cv2.cvtColor( LoG_image, cv2.COLOR_BGR2GRAY)
-    LoG_image = cv2.Laplacian( gray, cv2.CV_8U,3,3,2)       # parameter
+    LoG_image = cv2.Laplacian( gray, cv2.CV_8U,3,3,2)
     LoG_image = cv2.convertScaleAbs(LoG_image)
     return LoG_image
     
 def binarization(image):
     thresh = cv2.threshold(image,50,200,cv2.THRESH_BINARY_INV)[1]
-    #thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
     return thresh
 
 def preprocess_image(image):
@@ -57,27 +43,21 @@ def preprocess_image(image):
     image = binarization(image)
     return image
 
-# Find Signs
 def removeSmallComponents(image, threshold):
-    #find all your connected components (white blobs in your image)
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
     img2 = np.zeros((output.shape),dtype = np.uint8)
-    #for every component in the image, you keep it only if it's above threshold
     for i in range(0, nb_components):
         if sizes[i] >= threshold:
             img2[output == i + 1] = 255
     return img2
 
 def findContour(image):
-    #find contours in the thresholded image
     cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return cnts
 
 def contourIsSign(perimeter, centroid, threshold):
-    #  perimeter, centroid, threshold
-    # # Compute signature of contour
     result=[]
     for p in perimeter:
         p = p[0]
@@ -85,15 +65,13 @@ def contourIsSign(perimeter, centroid, threshold):
         result.append(distance)
     max_value = max(result)
     signature = [float(dist) / max_value for dist in result ]
-    # Check signature of contour.
     temp = sum((1 - s) for s in signature)
     temp = temp / len(signature)
-    if temp < threshold: # is  the sign
+    if temp < threshold:
         return True, max_value + 2
-    else:                 # is not the sign
+    else: 
         return False, max_value + 2
 
-#crop sign 
 def cropContour(image, center, max_distance):
     width = image.shape[1]
     height = image.shape[0]
@@ -111,7 +89,6 @@ def cropSign(image, coordinate):
     bottom = min([int(coordinate[1][1]), height-1])
     left = max([int(coordinate[0][0]), 0])
     right = min([int(coordinate[1][0]), width-1])
-    #print(top,left,bottom,right)
     return image[top:bottom,left:right]
 
 
@@ -140,7 +117,6 @@ def findSigns(image, contours, threshold, distance_theshold):
     signs = []
     coordinates = []
     for c in contours[0]:
-        # compute the center of the contour
         M = cv2.moments(c)
         if M["m00"] == 0:
             continue
@@ -159,18 +135,12 @@ def findSigns(image, contours, threshold, distance_theshold):
 def localization(image, min_size_components, similitary_contour_with_circle):
     original_image = image.copy()
     binary_image = preprocess_image(image)
-
     binary_image = removeSmallComponents(binary_image, min_size_components)
-
     binary_image = cv2.bitwise_and(binary_image,binary_image, mask=remove_other_color(image))
-
-    #binary_image = remove_line(binary_image)
-
     cv2.imshow('BINARY IMAGE', binary_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     contours = findContour(binary_image)
-    #signs, coordinates = findSigns(image, contours, similitary_contour_with_circle, 15)
     coordinate,sign = findLargestSign(original_image, contours, similitary_contour_with_circle, 15)
     return coordinate, original_image
 
@@ -190,32 +160,23 @@ def remove_line(img):
 def remove_other_color(img):
     frame = cv2.GaussianBlur(img, (3,3), 0) 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # define range of blue color in HSV
     lower_blue = np.array([100,128,0])
     upper_blue = np.array([215,255,255])
-    # Threshold the HSV image to get only blue colors
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-
     lower_white = np.array([0,0,128], dtype=np.uint8)
     upper_white = np.array([255,255,255], dtype=np.uint8)
-    # Threshold the HSV image to get only blue colors
     mask_white = cv2.inRange(hsv, lower_white, upper_white)
-
     lower_black = np.array([0,0,0], dtype=np.uint8)
     upper_black = np.array([170,150,50], dtype=np.uint8)
-
     mask_black = cv2.inRange(hsv, lower_black, upper_black)
-
     mask_1 = cv2.bitwise_or(mask_blue, mask_white)
     mask = cv2.bitwise_or(mask_1, mask_black)
-    # Bitwise-AND mask and original image
-    #res = cv2.bitwise_and(frame,frame, mask= mask)
     return mask
 
 def main():
     frame=cv2.imread('img1.jpg')
 
-    similitary_contour_with_circle = 0.60   # parameter
+    similitary_contour_with_circle = 0.60
     min_size_components=200
 
     width = frame.shape[1]
@@ -223,7 +184,6 @@ def main():
     dim = (width, height) 
     frame = cv2.resize(frame,dim,interpolation=cv2.INTER_AREA)
 
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     coordinate, image = localization(frame, min_size_components, similitary_contour_with_circle)
     width = image.shape[1]
     height = image.shape[0]
@@ -233,8 +193,6 @@ def main():
     b=coordinate[0][1]
     c=coordinate[1][0]
     d=coordinate[1][1]
-
-    #cv2.rectangle(image, coordinate[0],coordinate[1], (0,255, 0), 1)
 
     if coordinate[0][0]<0:
         a=0
@@ -257,8 +215,6 @@ def main():
     cv2.imshow('Result', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
 
 if __name__ == '__main__':
     main()
