f0d7a85432e95e7ae052aed2bdfc98747f1b0bf6
Sahil Mishra <sahilmishra0012@outlook.com>
2020-02-01 12:14:27 +0530

    New Changes

--- Image Segmentation Test Codes/samples/threshold_image.py
+++ Image Segmentation Test Codes/samples/threshold_image.py
@@ -2,14 +2,13 @@ import cv2
 import numpy as np
 img = cv2.imread("/home/samthekiller/Downloads/Smart India Hackathon/INTEL/Warning System for Drivers/Test Code/img1.jpg")
 img=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
-
+img = cv2.medianBlur(img, 3)
 Y,Cr,Cb = cv2.split(img)
 
 output_Y = cv2.equalizeHist(Y)
 output_Cr = cv2.equalizeHist(Cr)
 output_Cb = cv2.equalizeHist(Cb)
 img = cv2.merge((output_Y, output_Cr, output_Cb))
-9
 ret, thresh2 = cv2.threshold(output_Cr, 200, 255, cv2.THRESH_BINARY) 
 
 cv2.imshow('Image',thresh2)
