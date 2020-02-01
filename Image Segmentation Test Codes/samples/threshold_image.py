2efa068a3434c2c97425426412ea104ad9965d02
Sahil Mishra <sahilmishra0012@outlook.com>
2020-02-01 10:52:36 +0530

    New Changes

--- Image Segmentation Test Codes/samples/threshold_image.py
+++ Image Segmentation Test Codes/samples/threshold_image.py
@@ -9,7 +9,7 @@ output_Y = cv2.equalizeHist(Y)
 output_Cr = cv2.equalizeHist(Cr)
 output_Cb = cv2.equalizeHist(Cb)
 img = cv2.merge((output_Y, output_Cr, output_Cb))
-
+9
 ret, thresh2 = cv2.threshold(output_Cr, 200, 255, cv2.THRESH_BINARY) 
 
 cv2.imshow('Image',thresh2)
