43607b5bdef421de7d4195fe8e7bd5b18b217192
Sahil Mishra <sahilmishra0012@outlook.com>
2020-02-04 18:58:25 +0530

    New Changes

--- Bounding Box Test Codes/classify.py
+++ Bounding Box Test Codes/classify.py
@@ -3,9 +3,9 @@ import glob
 import cv2
 from dicttoxml import dicttoxml
 
-xml_file = '/home/samthekiller/Desktop/img.xml'
-img_file='/home/samthekiller/Desktop/img.jpg'
-filename='/home/samthekiller/Desktop/img1.jpg'
+xml_file = '/home/samthekiller/Desktop/000720_r.xml'
+img_file='/home/samthekiller/Desktop/000720_r.jpg'
+filename='/home/samthekiller/Desktop/000720_r1.jpg'
 x=xmltodict.parse(open( xml_file , 'rb' ))
 img=cv2.imread(img_file)
 width = int(img.shape[1] * 65 / 100)
@@ -20,13 +20,10 @@ for i in x['annotation']['object']:
         Ymaxvalue=int(int(i['bndbox']['ymax'])* 65 / 100)
         img = cv2.rectangle(img,(Xminvalue,Yminvalue),(Xmaxvalue,Ymaxvalue),(0,255,0),1)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        img=img[Xminvalue:Yminvalue,Xmaxvalue,Ymaxvalue]
-        cv2.imshow('Image Window',img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        cv2.putText(img,i['name'],(Xminvalue,Yminvalue), font, 0.5,(0,255,0),1,cv2.LINE_AA)
+        # cv2.putText(img,'Speed Limit - 40 Km/Hr',(Xminvalue,Yminvalue), font, 0.5,(0,255,0),1,cv2.LINE_AA)
 cv2.imshow('Image Window',img)
 cv2.waitKey(0)
 cv2.imwrite(filename, img) 
 cv2.destroyAllWindows()
+print('Speed Limit- 40 Km/Hr')
 
